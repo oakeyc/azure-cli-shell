4028862458ce97a6dbec50577072ad700c2c907b
Courtney Oka <oka.c@husky.neu.edu>
2017-04-20 14:19:48 -0700

    minor

--- test/test_gestures.py
+++ test/test_gestures.py
@@ -48,10 +48,6 @@ class CompletionTest(unittest.TestCase):
         if not os.path.exists(azure_folder):
             os.makedirs(azure_folder)
 
-        ACCOUNT.load(os.path.join(azure_folder, 'azureProfile.json'))
-        CONFIG.load(os.path.join(azure_folder, 'az.json'))
-        SESSION.load(os.path.join(azure_folder, 'az.sess'), max_age=3600)
-
         config = SHELL_CONFIGURATION
         shell_config_dir = azclishell.configuration.get_config_dir
 
